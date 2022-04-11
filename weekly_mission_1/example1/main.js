d1c0b1c5a8d0cd81bd43decb5fa6598fa6e2d8fe
SergioFarid <te.puy@hotmail.com>
2022-04-11 13:18:42 -0500

    prueba del primer cambio

---
+++ weekly_mission_1/example1/main.js
@@ -0,0 +1,9 @@
+
+// 1.- Creación de in objeto con propiedades
+
+
+let myCar = new Object(); // Creación de un objeto
+myCar.make = 'Ford'; // guardando valor dentro de un objeto creado
+myCar.model = 'Mustang';
+
+console.log("Que onda explorer's")
