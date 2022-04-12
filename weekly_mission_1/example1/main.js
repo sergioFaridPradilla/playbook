ed65274053594e050f4f527fd86b0efcc8533b9c
SergioFarid <te.puy@hotmail.com>
2022-04-11 22:22:46 -0500

    1. Ejercicio 1 elaborado

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -5,5 +5,29 @@
 let myCar = new Object(); // Creación de un objeto
 myCar.make = 'Ford'; // guardando valor dentro de un objeto creado
 myCar.model = 'Mustang';
+myCar.year = 1969;
 
-console.log("Que onda explorer's")
+console.log(myCar);  // imprimiendo objeto
+
+// 2. Declaración dde un objeto con variables locales y públicas
+
+const myModule = (() => {
+    
+    //variables de contexto local
+    const privateFoo = "Soy un valor privado, solo me usan dentro de este objeto";
+    const privateBar = [1,2,3,4];
+    const baz = "Soy un valor que va ser expuesto";
+
+    //variables para guardar llas variables locales
+    const exported = {
+        publicFoo: "Valor público, pueden verme desde donde me llamen",
+        publicBar: "Otro valor público",
+        publicBaz: baz
+    };
+
+    //Exposición de variables locales
+    return exported;
+
+}) 
+
+console.log(myModule())
