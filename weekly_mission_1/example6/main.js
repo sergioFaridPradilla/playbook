9af4d67e064bc62d9dc16d4381feb6bf32708472
SergioFarid <te.puy@hotmail.com>
2022-04-13 16:37:22 -0500

    6. Elaboración example6

---
+++ weekly_mission_1/example6/main.js
@@ -0,0 +1,5 @@
+
+require('./patcher') // Llamas este módulo que modifica el objeto instanciado
+const logger = require('./logger')  // Al llamar al módulo en logger.js te dará el objeto modificao.
+
+logger.customMessage();
