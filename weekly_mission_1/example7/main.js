8c67bae0f779e4bbe9859d43f22e03c8c8848934
SergioFarid <te.puy@hotmail.com>
2022-04-13 19:41:24 -0500

    7. Elaboración de example7

---
+++ weekly_mission_1/example7/main.js
@@ -0,0 +1,19 @@
+
+const Pokemon = require('./pokemon');
+
+const pikachu = new Pokemon("pikachu");
+const bulbasaur = new Pokemon("bilbasaur");
+const squirtle = new Pokemon("squirtle");
+const charmander = new Pokemon("charmander");
+
+pikachu.sayHello();
+pikachu.sayMessage("Heey");
+
+bulbasaur.sayHello();
+bulbasaur.sayMessage("Heeey!");
+
+charmander.sayHello();
+charmander.sayMessage("Heeey!");
+
+squirtle.sayHello()
+squirtle.sayMessage("Heeey!")
---
+++ weekly_mission_1/example7/pokemon.js
@@ -0,0 +1,17 @@
+
+class Pokemon {
+    constructor(name){
+
+        this.name = name;
+    }
+
+    sayHello() {
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+
+    sayMessage(msg){
+        console.log(`Mi pokemon ${this.name} dice: ${msg}`)
+    }
+}
+
+module.exports = Pokemon
