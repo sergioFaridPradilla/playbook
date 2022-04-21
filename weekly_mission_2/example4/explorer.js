e5ba69f0c9a89793cfc1797a1d84ad538f27c491
SergioFarid <te.puy@hotmail.com>
2022-04-20 20:22:20 -0500

    1. Elaboración example4_1

---
+++ weekly_mission_2/example4/explorer.js
@@ -0,0 +1,11 @@
+
+export default class explorer {
+    constructor(name, username, mission){
+        this.name
+        this.username
+        this.mission
+    }
+    getNameAndUsername(){
+        return `Explorer ${this.name}, username: ${this.username}`
+    }
+}
