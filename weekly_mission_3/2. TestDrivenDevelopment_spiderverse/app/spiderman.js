8d8cb5d4c50c35ba943eb07c9fb2e1c4a1630542
SergioFarid <te.puy@hotmail.com>
2022-05-02 14:40:17 -0500

    Class Spiderman se agregan los atributos faltantes

--- weekly_mission_3/2. TestDrivenDevelopment_spiderverse/app/spiderman.js
+++ weekly_mission_3/2. TestDrivenDevelopment_spiderverse/app/spiderman.js
@@ -1,11 +1,11 @@
 
 class Spiderman{
-    constructor(name){
+    constructor(name, age, actor, movies, studio){
         this.name = name
-    //     this.age = age,
-    //     this.actor = actor,
-    //     this.movies = movies,
-    //     this.studio = studio  , age, actor, movies, studio
+        this.age = age,
+        this.actor = actor,
+        this.movies = movies,
+        this.studio = studio  , age, actor, movies, studio
     } 
 }
 
