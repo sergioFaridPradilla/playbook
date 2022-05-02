24320fa3a8e067567ac1140af871d7c178aced00
SergioFarid <te.puy@hotmail.com>
2022-05-01 21:23:18 -0500

    Se creaa el archivo index y se instancia la clase MissionCommander de missioncommander.js

---
+++ weekly_mission_3/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require("./app/missionCommander");
+
+const sergio = new MissionCommander("Sergio")
+const karen = new MissionCommander("Karen")
+const maria = new MissionCommander("Maria")
+
+console.log(sergio.name);
+console.log(karen.name);
+console.log(maria.name);
