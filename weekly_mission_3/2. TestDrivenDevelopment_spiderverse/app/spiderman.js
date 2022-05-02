a81e589189f7802aea9561a7928affc657a2c9bb
SergioFarid <te.puy@hotmail.com>
2022-05-02 14:32:44 -0500

    Class Spiderman se coloca un atributo para pasar las pruebas e instanciar el objeto

---
+++ weekly_mission_3/2. TestDrivenDevelopment_spiderverse/app/spiderman.js
@@ -0,0 +1,12 @@
+
+class Spiderman{
+    constructor(name){
+        this.name = name
+    //     this.age = age,
+    //     this.actor = actor,
+    //     this.movies = movies,
+    //     this.studio = studio  , age, actor, movies, studio
+    } 
+}
+
+module.exports = Spiderman
