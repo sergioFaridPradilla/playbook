ccad28edba50607429763d2c24501ce64e1eeaf0
SergioFarid <te.puy@hotmail.com>
2022-05-05 11:25:17 -0500

    Paso 7: Se realiza Unit Test en ...user.test.js con el Objeto User y pasan exitosamente

--- weekly_mission_3/3. Twitter/test/models/user.test.js
+++ weekly_mission_3/3. Twitter/test/models/user.test.js
@@ -1,10 +1,15 @@
-const User = './../app/models/user'
+const User =  require('./../../app/models/user')
 
 describe("Unit Test for User Class", () => {
-    test('Test 1: ', ()=> {
+    test('Test 1: Create User Objet ', ()=> {
         const user = new User(1, "sergioF","Sergio Farid", "Bio", "dateCreated", "lastUpdate" )
 
         expect(user.id).toBe(1)
+        expect(user.userName).toBe("sergioF")
+        expect(user.name).toBe("Sergio Farid")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdate).toBe("lastUpdate")
     }) 
     
-} )
+})
