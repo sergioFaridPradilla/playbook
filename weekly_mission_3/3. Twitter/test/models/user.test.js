996372d55909c5dc572389421f7b96634c37434f
SergioFarid <te.puy@hotmail.com>
2022-05-05 23:05:12 -0500

    Paso 9: Se realiza el requerimiento 2 (Prueba y modificación )

--- weekly_mission_3/3. Twitter/test/models/user.test.js
+++ weekly_mission_3/3. Twitter/test/models/user.test.js
@@ -1,15 +1,19 @@
 const User =  require('./../../app/models/user')
 
 describe("Unit Test for User Class", () => {
-    test('Test 1: Create User Objet ', ()=> {
+    test('Requerimiento 1: Create User Objet ', ()=> {
         const user = new User(1, "sergioF","Sergio Farid", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.userName).toBe("sergioF")
         expect(user.name).toBe("Sergio Farid")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).not.toBeUndefined()
-        expect(user.lastUpdate).not.toBeUndefined()
     }) 
     
+    test('Requerimiento 2: Fechas en atributos de user', ()=>{
+        const user = new User(1, "sergioF","Sergio Farid", "Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdate).not.toBeUndefined()
+    })
+
 })
